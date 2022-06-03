34553974370588186bdc0ab9d9d48eea17c8b89b
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-03 18:40:51 +0200

    Added public list tattoos thunk test

--- src/pages/PublicListPage/PublicListPage.tsx
+++ src/pages/PublicListPage/PublicListPage.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import TattooItem from "../../components/TattooItem/TattooItem";
-import { useAppSelector } from "../../redux/hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks/hooks";
+import { loadTattoosThunk } from "../../redux/thunks/tattoosThunks/tattoosThunks";
 import { ITattoo } from "../../types/types";
 
 import PublicListPageStyled from "./PublicListPageStyled";
 
 const PublicListPage = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(loadTattoosThunk());
+  }, [dispatch]);
+
   const tattoos: ITattoo[] = useAppSelector((state) => state.tattoos);
 
   return (
---
+++ src/redux/thunks/tattoosThunks/tattoosThunks.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { loadTattoosActionCreator } from "../../features/tattoosSlice/tattoosSlice";
+import { AppDispatch } from "../../store/store";
+
+export const loadTattoosThunk = () => async (dispatch: AppDispatch) => {
+  const {
+    data: { tattoos },
+  } = await axios.get(`${process.env.REACT_APP_API_URL}/tattoos/list`);
+
+  dispatch(loadTattoosActionCreator(tattoos));
+};
