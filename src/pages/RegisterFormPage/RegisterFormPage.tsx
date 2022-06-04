8ab482775ea85d6c0a3e1c4ef641d6bc813ce0b0
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-04 20:38:47 +0200

    Created RegisterFormPageb

---
+++ src/pages/RegisterFormPage/RegisterFormPage.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+const RegisterFormPage = (): JSX.Element => {
+  return (
+    <>
+      <h2 className="page-title">Register in TooTattoo</h2>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default RegisterFormPage;
