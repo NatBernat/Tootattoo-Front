c5c356af1f4d6856ad70771d20677a00be8da831
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-16 00:56:03 +0200

    Created again mockedList to avoid duplications

--- src/mocks/mockTattoos.ts
+++ src/mocks/mockTattoos.ts
@@ -23,84 +23,139 @@ const mockTattoos = [
 
 export const mockedTattoos = [
   {
-    _id: "1111111",
-    title: "Arm avstract fluid forms",
-    image:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
+    _id: "62a842836e5ac2e9dc0d7887",
+    title: "poketattoo",
+    image: "uploads/images/1655220326956E3WmND7XEAMNJhf.jpg",
     imageBackup:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["small", "blackwork", "b/n", "photo"],
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220326956E3WmND7XEAMNJhf.jpg?alt=media&token=332f7d9a-559b-48f9-bd60-36460bd036b6",
+    creator: "gromenauer",
+    creationDate: "2022-6-14",
+    tags: [],
   },
   {
-    _id: "22222222",
-    title: "Coloret realistic flowers",
-    image: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    imageBackup: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["medium", "traditional", "color", "photo"],
-  },
-  {
-    _id: "3333333",
-    title: "Arm abstract fluid forms",
-    image:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
+    _id: "62a845c4d834e5a1b6e71962",
+    title: "Mononoke princess colorful",
+    image: "uploads/images/1655220423058DtfVEluXoAEA2Oj.jpg",
     imageBackup:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["small", "blackwork", "b/n", "photo"],
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220423058DtfVEluXoAEA2Oj.jpg?alt=media&token=838f52ee-d501-4eff-8196-f51ac3a0286b",
+    creator: "natbernat",
+    creationDate: "2022-6-14",
+    tags: [],
   },
   {
-    _id: "4444444",
-    title: "Colored realistic flowers",
-    image: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    imageBackup: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2020-06-02",
-    tags: ["medium", "traditional", "color", "photo"],
-  },
-  {
-    _id: "55555555",
-    title: "Arm abstract fluit forms",
+    _id: "62a87b6b6f46bed89b6c2d57",
+
+    title: "floral blackwork arm",
     image:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
+      "uploads/images/1655220384033tumblr_698d2677f7bc94390814c986e6c46d0b_b524281c_1280.jpg",
     imageBackup:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2021-06-02",
-    tags: ["small", "blackwork", "b/n", "photo"],
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220384033tumblr_698d2677f7bc94390814c986e6c46d0b_b524281c_1280.jpg?alt=media&token=cb19a607-697b-4e81-98ae-35b2ad70b972",
+    creator: "gromenauer",
+    creationDate: "2022-6-14",
+    tags: [],
   },
   {
-    _id: "2a2b2c",
-    title: "Colored realistic flowers",
-    image: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    imageBackup: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["medium", "traditional", "color", "photo"],
-  },
-  {
-    _id: "1a1b1c",
-    title: "Arm abstract fluid forms",
-    image:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
+    _id: "62a8966506e000ce88f2f02e",
+    title: "palmera",
+    image: "uploads/images/1655215716614Usar-esta-de-portada.png",
     imageBackup:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["small", "blackwork", "b/n", "photo"],
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655215716614Usar-esta-de-portada.png?alt=media&token=99f7311b-3507-4fc8-896c-4ce0fea1853a",
+    creator: "pepe",
+    creationDate: "2022-6-14",
+    tags: [],
   },
   {
-    _id: "2a2b2c",
-    title: "Colored realistic flowers",
-    image: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    imageBackup: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["medium", "traditional", "color", "photo"],
+    _id: "62a8970306e000ce88f2f032",
+    title: "flowers",
+    image: "uploads/images/1655215874980images.jpeg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655215874980images.jpeg?alt=media&token=7146f136-c467-4428-a770-4a088cf53bea",
+    creator: "pepe",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62a8a98dd1c80dd22f4a573a",
+
+    title: "Abstract black bodyswit",
+    image: "uploads/images/1655220620916tumblr_pq2t4hwq1c1wofdv7o1_640.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220620916tumblr_pq2t4hwq1c1wofdv7o1_640.jpg?alt=media&token=65737690-5ff1-4892-a0fd-bb1d6cdd19e5",
+    creator: "natbernat",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62a8a9acd1c80dd22f4a5741",
+    title: "Police small old-school",
+    image: "uploads/images/1655220651291l.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220651291l.jpg?alt=media&token=cc4d18b3-b62e-49ff-af19-19cf36c0f413",
+    creator: "gromenauer",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62a8a9e4d1c80dd22f4a5748",
+
+    title: "Geometric full arm",
+    image: "uploads/images/1655220707654110719-1623-4824.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220707654110719-1623-4824.jpg?alt=media&token=c2d56a7f-7213-41b4-9487-adc921ed88e6",
+    creator: "gromenauer",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62a8aa16d1c80dd22f4a5751",
+    title: "Abstract blackwork arm",
+    image: "uploads/images/1655220757698e8f70c51540684a4daf881a0cb73ed42.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655220757698e8f70c51540684a4daf881a0cb73ed42.jpg?alt=media&token=4338050c-6a55-43f7-95cc-a78b658b8fc3",
+    creator: "mario",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62a8aba9d1c80dd22f4a5765",
+    title: "cute colored grogu",
+    image: "uploads/images/1655330578529Grogu.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655330578529Grogu.jpg?alt=media&token=4112d4e5-9532-47fe-a222-6ed421e36cb4",
+    creator: "mario",
+    creationDate: "2022-6-14",
+    tags: [],
+  },
+  {
+    _id: "62aa59a8d58b1ef5e25986a3",
+    title: "cute b/w pig",
+    image: "uploads/images/1655331240375cute pig.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655331240375cute%20pig.jpg?alt=media&token=7d9c5904-0c83-49ea-b8ba-98a9746fa1cf",
+    creator: "natbernat",
+    creationDate: "2022-6-16",
+    tags: [],
+  },
+  {
+    _id: "62aa5b5bd58b1ef5e25986a9",
+
+    title: "antifa snake b/w",
+    image: "uploads/images/1655331674981antifa snake b-w.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655331674981antifa%20snake%20b-w.jpg?alt=media&token=acf112e9-50e5-4257-a725-9ad92719939d",
+    creator: "mario",
+    creationDate: "2022-6-16",
+    tags: [],
+  },
+  {
+    _id: "62aa5ca6d58b1ef5e25986b5",
+    title: "colorful tiny of the street face",
+    image: "uploads/images/1655332006066chiquito.jpg",
+    imageBackup:
+      "https://firebasestorage.googleapis.com/v0/b/tootattoo-images.appspot.com/o/1655332006066chiquito.jpg?alt=media&token=98f80c9c-22c3-4334-9826-96a49d8503a7",
+    creator: "gromenauer",
+    creationDate: "2022-6-16",
+    tags: [],
   },
 ];
 
