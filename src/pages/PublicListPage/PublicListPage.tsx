399068e0cc8677f847ebe7dd18597419d7792447
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-15 17:37:50 +0200

    Added cypress test for register and login

--- src/pages/PublicListPage/PublicListPage.tsx
+++ src/pages/PublicListPage/PublicListPage.tsx
@@ -41,42 +41,41 @@ const PublicListPage = (): JSX.Element => {
             return <TattooItem key={tattoo._id} tattoo={tattoo} />;
           })}
         </ul>
-        {tattoos.length > 5 && (
-          <div className="pagination" onClick={scrollUp}>
-            {index > 0 && (
-              <button
-                onClick={() => {
-                  if (index >= 6) {
-                    setIndex(index - 6);
-                  }
-                }}
-              >
-                <img
-                  className="button__icon--back"
-                  src="./images/arrow-icon.svg"
-                  alt="back"
-                  title="back"
-                />
-              </button>
-            )}
-            {tattoos.length > index + 6 && (
-              <button
-                onClick={() => {
-                  if (index < tattoos.length - 6) {
-                    setIndex(index + 6);
-                  }
-                }}
-              >
-                <img
-                  className="button__icon--next"
-                  src="./images/arrow-icon.svg"
-                  alt="next"
-                  title="next"
-                />
-              </button>
-            )}
-          </div>
-        )}
+
+        <div className="pagination" onClick={scrollUp}>
+          {index > 0 && (
+            <button
+              onClick={() => {
+                if (index >= 6) {
+                  setIndex(index - 6);
+                }
+              }}
+            >
+              <img
+                className="button__icon--back"
+                src="./images/arrow-icon.svg"
+                alt="back"
+                title="back"
+              />
+            </button>
+          )}
+          {tattoos.length > index + 6 && (
+            <button
+              onClick={() => {
+                if (index < tattoos.length - 6) {
+                  setIndex(index + 6);
+                }
+              }}
+            >
+              <img
+                className="button__icon--next"
+                src="./images/arrow-icon.svg"
+                alt="next"
+                title="next"
+              />
+            </button>
+          )}
+        </div>
       </PublicListPageStyled>
     </>
   );
