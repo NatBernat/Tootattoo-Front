532862701782b77c911aff34ef6baa5b02908a29
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-15 13:29:57 +0200

    Added snapshots tests

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import TestRenderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
 import NotFoundPage from "./NotFoundPage";
 
 describe("Given NotFoundPage component", () => {
-  describe("When it's instantiated", () => {
+  describe("When it's invoked", () => {
     test("Then it should render a paragraph", () => {
       const expectedText = "Page not found";
 
@@ -17,6 +18,18 @@ describe("Given NotFoundPage component", () => {
       const testedParagraph = screen.getByText(expectedText);
 
       expect(testedParagraph).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered", () => {
+    test("Then it should always match this snapshot", () => {
+      const LoadingComponent = TestRenderer.create(
+        <Provider store={store}>
+          <NotFoundPage />
+        </Provider>
+      );
+
+      expect(LoadingComponent).toMatchSnapshot();
     });
   });
 });
