7f7979fcd69977c0e109d265625a22b98bff0b31
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-06-16 01:17:02 +0200

    Removed commented lines

--- src/mocks/mockTattoos.ts
+++ src/mocks/mockTattoos.ts
@@ -1,26 +1,3 @@
-/* const mockTattoos = [
-  {
-    _id: "1a1b1c",
-    title: "Arm abstract fluid forms",
-    image:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    imageBackup:
-      "https://i.pinimg.com/564x/e8/f7/0c/e8f70c51540684a4daf881a0cb73ed42.jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["small", "blackwork", "b/n", "photo"],
-  },
-  {
-    _id: "2a2b2c",
-    title: "Colored realistic flowers",
-    image: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    imageBackup: "https://pbs.twimg.com/media/FTW9Z45aAAAz67v?format=jpg",
-    creator: "6295aacd280fd64f7583019f",
-    creationDate: "2022-06-02",
-    tags: ["medium", "traditional", "color", "photo"],
-  },
-]; */
-
 const mockTattoos = [
   {
     _id: "62a842836e5ac2e9dc0d7887",
